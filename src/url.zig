318ffc1903506838837877015a6ab6ab311712ec
pchchv <ipchchv@gmail.com>
2024-08-13 20:13:18 +0300

    Refactoring

--- src/url.zig
+++ src/url.zig
@@ -1,5 +1,21 @@
 const std = @import("std");
 
+const HEX_CHAR = blk: {
+    var all = std.mem.zeroes([255]bool);
+    for ('a'..('f' + 1)) |b| all[b] = true;
+    for ('A'..('F' + 1)) |b| all[b] = true;
+    for ('0'..('9' + 1)) |b| all[b] = true;
+    break :blk all;
+};
+
+const HEX_DECODE = blk: {
+    var all = std.mem.zeroes([255]u8);
+    for ('a'..('z' + 1)) |b| all[b] = b - 'a' + 10;
+    for ('A'..('Z' + 1)) |b| all[b] = b - 'A' + 10;
+    for ('0'..('9' + 1)) |b| all[b] = b - '0';
+    break :blk all;
+};
+
 pub const Url = struct {
     raw: []const u8 = "",
     path: []const u8 = "",
@@ -59,20 +75,4 @@ pub const Url = struct {
 
         return true;
     }
-};
-
-const HEX_CHAR = blk: {
-    var all = std.mem.zeroes([255]bool);
-    for ('a'..('f' + 1)) |b| all[b] = true;
-    for ('A'..('F' + 1)) |b| all[b] = true;
-    for ('0'..('9' + 1)) |b| all[b] = true;
-    break :blk all;
-};
-
-const HEX_DECODE = blk: {
-    var all = std.mem.zeroes([255]u8);
-    for ('a'..('z' + 1)) |b| all[b] = b - 'a' + 10;
-    for ('A'..('Z' + 1)) |b| all[b] = b - 'A' + 10;
-    for ('0'..('9' + 1)) |b| all[b] = b - '0';
-    break :blk all;
 };
