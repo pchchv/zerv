02c55b78fda6e07b6714bd00f44fc5d87093cb70
pchchv <ipchchv@gmail.com>
2024-08-13 19:57:05 +0300

    Implement a parse method

--- src/url.zig
+++ src/url.zig
@@ -1,5 +1,21 @@
+const std = @import("std");
+
 pub const Url = struct {
     raw: []const u8 = "",
     path: []const u8 = "",
     query: []const u8 = "",
-};
+
+    pub fn parse(raw: []const u8) Url {
+        var path = raw;
+        var query: []const u8 = "";
+        if (std.mem.indexOfScalar(u8, raw, '?')) |index| {
+            path = raw[0..index];
+            query = raw[index + 1 ..];
+        }
+
+        return .{
+            .raw = raw,
+            .path = path,
+            .query = query,
+        };
+    }
