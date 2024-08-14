ed4542acd87f88db5813d6d4727f367ff6356f3f
pchchv <ipchchv@gmail.com>
2024-08-14 12:51:06 +0300

    Implement init and deinit methods

--- src/key_value.zig
+++ src/key_value.zig
@@ -1,7 +1,25 @@
+const std = @import("std");
+
+const Allocator = std.mem.Allocator;
+
 fn MakeKeyValue(K: type, V: type, equalFn: fn (lhs: K, rhs: K) bool) type {
     return struct {
         len: usize,
         keys: []K,
         values: []V,
+        const Self = @This();
+
+        pub fn init(allocator: Allocator, max: usize) !Self {
+            return .{
+                .len = 0,
+                .keys = try allocator.alloc(K, max),
+                .values = try allocator.alloc(V, max),
+            };
+        }
+
+        pub fn deinit(self: *Self, allocator: Allocator) void {
+            allocator.free(self.keys);
+            allocator.free(self.values);
+        }
     };
 }
