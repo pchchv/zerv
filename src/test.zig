9a0a93a4b19ac08d9b386c42c1fcc77060b0001f
pchchv <ipchchv@gmail.com>
2024-08-14 12:01:17 +0300

    Implement expectEqual function

--- src/test.zig
+++ src/test.zig
@@ -3,3 +3,7 @@ const std = @import("std");
 
 pub const expectError = std.testing.expectError;
 pub const expectString = std.testing.expectEqualStrings;
+
+pub fn expectEqual(expected: anytype, actual: anytype) !void {
+    try std.testing.expectEqual(@as(@TypeOf(actual), expected), actual);
+}
