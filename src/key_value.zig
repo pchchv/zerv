08204c031096bc00872761530dee7f1df5dd24ba
pchchv <ipchchv@gmail.com>
2024-08-14 12:52:09 +0300

    Implement add and get methods

--- src/key_value.zig
+++ src/key_value.zig
@@ -21,5 +21,25 @@ fn MakeKeyValue(K: type, V: type, equalFn: fn (lhs: K, rhs: K) bool) type {
             allocator.free(self.keys);
             allocator.free(self.values);
         }
-    };
-}
+
+        pub fn add(self: *Self, key: K, value: V) void {
+            const len = self.len;
+            var keys = self.keys;
+            if (len == keys.len) {
+                return;
+            }
+
+            keys[len] = key;
+            self.values[len] = value;
+            self.len = len + 1;
+        }
+
+        pub fn get(self: *const Self, needle: K) ?V {
+            const keys = self.keys[0..self.len];
+            for (keys, 0..) |key, i| {
+                if (equalFn(key, needle)) {
+                    return self.values[i];
+                }
+            }
+            return null;
+        }
