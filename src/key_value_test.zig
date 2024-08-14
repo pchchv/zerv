1eead9c8feb9e148a4650180dd02d08d90294bfb
pchchv <ipchchv@gmail.com>
2024-08-14 13:07:42 +0300

    Add KeyValue and MultiForm "ignores beyond max" tests

--- src/key_value_test.zig
+++ src/key_value_test.zig
@@ -19,6 +19,39 @@ test "KeyValue: get" {
     try t.expectEqual("application/json2", kv.get("content-type").?);
 }
 
+test "KeyValue: ignores beyond max" {
+    var kv = try KeyValue.init(t.allocator, 2);
+    defer kv.deinit(t.allocator);
+    var n1 = "content-length".*;
+    kv.add(&n1, "cl");
+
+    var n2 = "host".*;
+    kv.add(&n2, "www");
+
+    var n3 = "authorization".*;
+    kv.add(&n3, "hack");
+
+    try t.expectEqual("cl", kv.get("content-length").?);
+    try t.expectEqual("www", kv.get("host").?);
+    try t.expectEqual(null, kv.get("authorization"));
+
+    {
+        var it = kv.iterator();
+        {
+            const field = it.next().?;
+            try t.expectString("content-length", field.key);
+            try t.expectString("cl", field.value);
+        }
+
+        {
+            const field = it.next().?;
+            try t.expectString("host", field.key);
+            try t.expectString("www", field.value);
+        }
+        try t.expectEqual(null, it.next());
+    }
+}
+
 test "MultiFormKeyValue: get" {
     var kv = try MultiFormKeyValue.init(t.allocator, 2);
     defer kv.deinit(t.allocator);
@@ -32,4 +65,38 @@ test "MultiFormKeyValue: get" {
     try t.expectEqual(null, kv.get("username"));
     kv.add(&key, .{ .value = "leto2" });
     try t.expectEqual("leto2", kv.get("username").?.value);
+}
+
+test "MultiFormKeyValue: ignores beyond max" {
+    var kv = try MultiFormKeyValue.init(t.allocator, 2);
+    defer kv.deinit(t.allocator);
+
+    var n1 = "username".*;
+    kv.add(&n1, .{ .value = "leto" });
+
+    var n2 = "password".*;
+    kv.add(&n2, .{ .value = "ghanima" });
+
+    var n3 = "other".*;
+    kv.add(&n3, .{ .value = "hack" });
+
+    try t.expectEqual("leto", kv.get("username").?.value);
+    try t.expectEqual("ghanima", kv.get("password").?.value);
+    try t.expectEqual(null, kv.get("other"));
+
+    {
+        var it = kv.iterator();
+        {
+            const field = it.next().?;
+            try t.expectString("username", field.key);
+            try t.expectString("leto", field.value.value);
+        }
+
+        {
+            const field = it.next().?;
+            try t.expectString("password", field.key);
+            try t.expectString("ghanima", field.value.value);
+        }
+        try t.expectEqual(null, it.next());
+    }
 }
