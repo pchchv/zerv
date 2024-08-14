aba169769ab163b5548d629f48e5526c9089a907
pchchv <ipchchv@gmail.com>
2024-08-14 13:07:03 +0300

    Add KeyValue and MultiForm get tests

---
+++ src/key_value_test.zig
@@ -0,0 +1,35 @@
+const t = @import("t.zig");
+const kval = @import("key_value.zig");
+
+const KeyValue = kval.KeyValue;
+const MultiFormKeyValue = kval.MultiFormKeyValue;
+
+test "KeyValue: get" {
+    var kv = try KeyValue.init(t.allocator, 2);
+    defer kv.deinit(t.allocator);
+
+    var key = "content-type".*;
+    kv.add(&key, "application/json");
+
+    try t.expectEqual("application/json", kv.get("content-type").?);
+
+    kv.reset();
+    try t.expectEqual(null, kv.get("content-type"));
+    kv.add(&key, "application/json2");
+    try t.expectEqual("application/json2", kv.get("content-type").?);
+}
+
+test "MultiFormKeyValue: get" {
+    var kv = try MultiFormKeyValue.init(t.allocator, 2);
+    defer kv.deinit(t.allocator);
+
+    var key = "username".*;
+    kv.add(&key, .{ .value = "leto" });
+
+    try t.expectEqual("leto", kv.get("username").?.value);
+
+    kv.reset();
+    try t.expectEqual(null, kv.get("username"));
+    kv.add(&key, .{ .value = "leto2" });
+    try t.expectEqual("leto2", kv.get("username").?.value);
+}
