72997b8e86e2a000820031e2bed3f5b8650a57a0
pchchv <ipchchv@gmail.com>
2024-08-15 10:50:43 +0300

    Implement a randomString func

--- src/test.zig
+++ src/test.zig
@@ -7,3 +7,12 @@ pub const expectString = std.testing.expectEqualStrings;
 pub fn expectEqual(expected: anytype, actual: anytype) !void {
     try std.testing.expectEqual(@as(@TypeOf(actual), expected), actual);
 }
+
+pub fn randomString(random: std.Random, a: std.mem.Allocator, max: usize) []u8 {
+    var buf = a.alloc(u8, random.uintAtMost(usize, max) + 1) catch unreachable;
+    const valid = "abcdefghijklmnopqrstuvwxyz0123456789-_";
+    for (0..buf.len) |i| {
+        buf[i] = valid[random.uintAtMost(usize, valid.len - 1)];
+    }
+    return buf;
+}
