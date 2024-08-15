b1906731ccc43619dc399b58dcf4912c590ee8d8
pchchv <ipchchv@gmail.com>
2024-08-15 10:15:18 +0300

    Implement a Request

---
+++ src/request.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+const zerv = @import("zerv.zig");
+const buffer = @import("buffer.zig");
+
+const Url = @import("url.zig").Url;
+const Params = @import("params.zig").Params;
+const KeyValue = @import("key_value.zig").KeyValue;
+const MultiFormKeyValue = @import("key_value.zig").MultiFormKeyValue;
+
+const Address = std.net.Address;
+const Allocator = std.mem.Allocator;
+
+pub const Request = struct {
+    // The URL of the request
+    url: Url,
+
+    // the address of the client
+    address: Address,
+
+    // Path params (extracted from the URL based on the route).
+    // Using req.param(NAME) is preferred.
+    params: Params,
+
+    // The headers of the request. Using req.header(NAME) is preferred.
+    headers: KeyValue,
+
+    // The request method.
+    method: zerv.Method,
+
+    // The request protocol.
+    protocol: zerv.Protocol,
+
+    // The body of the request, if any.
+    body_buffer: ?buffer.Buffer = null,
+    body_len: usize = 0,
+
+    // cannot use an optional on qs, because it's pre-allocated so always exists
+    qs_read: bool = false,
+
+    // The query string lookup.
+    qs: KeyValue,
+
+    // cannot use an optional on fd, because it's pre-allocated so always exists
+    fd_read: bool = false,
+
+    // The formData lookup.
+    fd: KeyValue,
+
+    // The multiFormData lookup.
+    mfd: MultiFormKeyValue,
+
+    // Spare space we still have in our static buffer after parsing the request
+    // We can use this, if needed, for example to unescape querystring parameters
+    spare: []u8,
+
+    // An arena that will be reset at the end of each request. Can be used
+    // internally by this framework. The application is also free to make use of
+    // this arena. This is the same arena as response.arena.
+    arena: Allocator,
+};
