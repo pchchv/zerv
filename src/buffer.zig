e31e0f102229c633839705111262fb5ffe984ef3
pchchv <ipchchv@gmail.com>
2024-08-15 10:22:32 +0300

    Implement a Pool

--- src/buffer.zig
+++ src/buffer.zig
@@ -1,3 +1,10 @@
+const std = @import("std");
+
+const blockingMode = @import("httpz.zig").blockingMode;
+
+const Mutex = std.Thread.Mutex;
+const Allocator = std.mem.Allocator;
+
 pub const Buffer = struct {
     data: []u8,
     type: Type,
@@ -8,4 +15,20 @@ pub const Buffer = struct {
         pooled,
         dynamic,
     };
+};
+
+/// Pool is shared by threads in the blocking worker's thread pool when we are in blocking mode.
+/// Thus, needed to synchronize accesses.
+/// When we are not in locking mode,
+/// each worker gets its own Pool,
+/// and the pool is accessed only from that worker thread,
+///  so no locking is required.
+pub const Pool = struct {
+    const M = if (blockingMode()) Mutex else void;
+
+    available: usize,
+    buffers: []Buffer,
+    allocator: Allocator,
+    buffer_size: usize,
+    mutex: M,
 };
