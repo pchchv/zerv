63f8f1e9756d9bb07c14a648834e36cbbb28c3e4
pchchv <ipchchv@gmail.com>
2024-08-15 10:17:59 +0300

    Implement a blockingMode

--- src/zerv.zig
+++ src/zerv.zig
@@ -1,3 +1,8 @@
+const build = @import("build");
+const builtin = @import("builtin");
+
+const force_blocking: bool = if (@hasDecl(build, "httpz_blocking")) build.httpz_blocking else false;
+
 pub const Protocol = enum {
     HTTP10,
     HTTP11,
@@ -12,3 +17,13 @@ pub const Method = enum {
     DELETE,
     OPTIONS,
 };
+
+pub fn blockingMode() bool {
+    if (force_blocking) {
+        return true;
+    }
+    return switch (builtin.os.tag) {
+        .linux, .macos, .ios, .tvos, .watchos, .freebsd, .netbsd, .dragonfly, .openbsd => false,
+        else => true,
+    };
+}
