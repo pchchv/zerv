273e220aa1651ce7ffbf8ed2956dc7c9bd970375
pchchv <ipchchv@gmail.com>
2024-08-15 10:52:33 +0300

    Implement reset and getRandom functions

--- src/test.zig
+++ src/test.zig
@@ -3,9 +3,16 @@ const std = @import("std");
 
 pub const expectError = std.testing.expectError;
 pub const expectString = std.testing.expectEqualStrings;
+pub const allocator = std.testing.allocator;
+
+pub var arena = std.heap.ArenaAllocator.init(allocator);
 
 pub fn expectEqual(expected: anytype, actual: anytype) !void {
     try std.testing.expectEqual(@as(@TypeOf(actual), expected), actual);
+}
+
+pub fn reset() void {
+    _ = arena.reset(.free_all);
 }
 
 pub fn randomString(random: std.Random, a: std.mem.Allocator, max: usize) []u8 {
@@ -15,4 +22,10 @@ pub fn randomString(random: std.Random, a: std.mem.Allocator, max: usize) []u8 {
         buf[i] = valid[random.uintAtMost(usize, valid.len - 1)];
     }
     return buf;
+}
+
+pub fn getRandom() std.Random.DefaultPrng {
+    var seed: u64 = undefined;
+    std.posix.getrandom(std.mem.asBytes(&seed)) catch unreachable;
+    return std.Random.DefaultPrng.init(seed);
 }
