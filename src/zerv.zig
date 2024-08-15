690217196c01a692eb854f627bb426fcc86a0157
pchchv <ipchchv@gmail.com>
2024-08-15 10:45:48 +0300

    Implement a forExtension method

--- src/zerv.zig
+++ src/zerv.zig
@@ -1,5 +1,10 @@
+const std = @import("std");
 const build = @import("build");
 const builtin = @import("builtin");
+
+const url = @import("url.zig");
+
+const asUint = url.asUint;
 
 const force_blocking: bool = if (@hasDecl(build, "httpz_blocking")) build.httpz_blocking else false;
 
@@ -44,6 +49,66 @@ pub const ContentType = enum {
     WOFF2,
     XML,
     UNKNOWN,
+
+    pub fn forExtension(ext: []const u8) ContentType {
+        if (ext.len == 0) return .UNKNOWN;
+        const temp = if (ext[0] == '.') ext[1..] else ext;
+        if (temp.len > 5) return .UNKNOWN;
+
+        var normalized: [5]u8 = undefined;
+        for (temp, 0..) |c, i| {
+            normalized[i] = std.ascii.toLower(c);
+        }
+
+        switch (temp.len) {
+            2 => {
+                switch (@as(u16, @bitCast(normalized[0..2].*))) {
+                    asUint("js") => return .JS,
+                    asUint("gz") => return .GZ,
+                    else => return .UNKNOWN,
+                }
+            },
+            3 => {
+                switch (@as(u24, @bitCast(normalized[0..3].*))) {
+                    asUint("css") => return .CSS,
+                    asUint("csv") => return .CSV,
+                    asUint("eot") => return .EOT,
+                    asUint("gif") => return .GIF,
+                    asUint("htm") => return .HTML,
+                    asUint("ico") => return .ICO,
+                    asUint("jpg") => return .JPG,
+                    asUint("otf") => return .OTF,
+                    asUint("pdf") => return .PDF,
+                    asUint("png") => return .PNG,
+                    asUint("svg") => return .SVG,
+                    asUint("tar") => return .TAR,
+                    asUint("ttf") => return .TTF,
+                    asUint("xml") => return .XML,
+                    else => return .UNKNOWN,
+                }
+            },
+            4 => {
+                switch (@as(u32, @bitCast(normalized[0..4].*))) {
+                    asUint("jpeg") => return .JPG,
+                    asUint("json") => return .JSON,
+                    asUint("html") => return .HTML,
+                    asUint("text") => return .TEXT,
+                    asUint("wasm") => return .WASM,
+                    asUint("woff") => return .WOFF,
+                    asUint("webp") => return .WEBP,
+                    else => return .UNKNOWN,
+                }
+            },
+            5 => {
+                switch (@as(u40, @bitCast(normalized[0..5].*))) {
+                    asUint("woff2") => return .WOFF2,
+                    else => return .UNKNOWN,
+                }
+            },
+            else => return .UNKNOWN,
+        }
+        return .UNKNOWN;
+    }
 };
 
 pub fn blockingMode() bool {
