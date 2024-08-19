31a23eb14d168344b0c5cea0a767462cefc8b8a0
pchchv <ipchchv@gmail.com>
2024-08-19 06:59:32 +0300

    Add a write and content_type response tests

--- src/response_test.zig
+++ src/response_test.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const t = @import("test.zig");
+const zerv = @import("zerv.zig");
 const r = @import("response.zig");
 
 test "writeInt" {
@@ -11,4 +12,36 @@ test "writeInt" {
         const l = r.writeInt(&buf, @intCast(i));
         try t.expectString(tst[0..expected_len], buf[0..l]);
     }
+}
+
+test "response: write" {
+    var ctx = t.Context.init(.{});
+    defer ctx.deinit();
+
+    {
+        // no body
+        var res = ctx.response();
+        res.status = 401;
+        try res.write();
+        try ctx.expect("HTTP/1.1 401 \r\nContent-Length: 0\r\n\r\n");
+    }
+
+    {
+        // body
+        var res = ctx.response();
+        res.status = 200;
+        res.body = "hello";
+        try res.write();
+        try ctx.expect("HTTP/1.1 200 \r\nContent-Length: 5\r\n\r\nhello");
+    }
+}
+
+test "response: content_type" {
+    var ctx = t.Context.init(.{});
+    defer ctx.deinit();
+
+    var res = ctx.response();
+    res.content_type = zerv.ContentType.WEBP;
+    try res.write();
+    try ctx.expect("HTTP/1.1 200 \r\nContent-Type: image/webp\r\nContent-Length: 0\r\n\r\n");
 }
