8ba42c446ca34e08a560cd9b8a5f058cbd73ded6
pchchv <ipchchv@gmail.com>
2024-08-23 14:39:28 +0300

    Implement a Config

---
+++ src/router.zig
@@ -0,0 +1,9 @@
+const zerv = @import("zerv.zig");
+
+pub fn Config(comptime Handler: type, comptime Action: type) type {
+    const Dispatcher = zerv.Dispatcher(Handler, Action);
+    return struct {
+        handler: ?Handler = null,
+        dispatcher: ?Dispatcher = null,
+    };
+}
