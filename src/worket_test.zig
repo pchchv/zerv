1fddbfcaf023017037062b83de0f744f62dd4820
pchchv <ipchchv@gmail.com>
2024-08-23 14:34:02 +0300

    Implement a tersts of a HTTPConnPool

---
+++ src/worket_test.zig
@@ -0,0 +1,31 @@
+const t = @import("test.zig");
+const w = @import("worker.zig");
+
+const BufferPool = @import("buffer.zig").Pool;
+
+test "HTTPConnPool" {
+    var bp = try BufferPool.init(t.allocator, 2, 64);
+    defer bp.deinit();
+
+    var p = try w.HTTPConnPool.init(t.allocator, &bp, undefined, &.{
+        .workers = .{ .min_conn = 2 },
+        .request = .{ .buffer_size = 64 },
+    });
+    defer p.deinit();
+
+    const s1 = try p.acquire();
+    const s2 = try p.acquire();
+    const s3 = try p.acquire();
+
+    try t.expectEqual(true, s1.req_state.buf.ptr != s2.req_state.buf.ptr);
+    try t.expectEqual(true, s1.req_state.buf.ptr != s3.req_state.buf.ptr);
+    try t.expectEqual(true, s2.req_state.buf.ptr != s3.req_state.buf.ptr);
+
+    p.release(s2);
+    const s4 = try p.acquire();
+    try t.expectEqual(true, s4.req_state.buf.ptr == s2.req_state.buf.ptr);
+
+    p.release(s1);
+    p.release(s3);
+    p.release(s4);
+}
