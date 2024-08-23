93b4ec679429ceb29ff23cd6c8437a893c6d9403
pchchv <ipchchv@gmail.com>
2024-08-23 14:44:41 +0300

    Implement a Part

--- src/router.zig
+++ src/router.zig
@@ -1,9 +1,26 @@
+const std = @import("std");
+
 const zerv = @import("zerv.zig");
+
+const StringHashMap = std.StringHashMap;
 
 pub fn Config(comptime Handler: type, comptime Action: type) type {
     const Dispatcher = zerv.Dispatcher(Handler, Action);
     return struct {
         handler: ?Handler = null,
         dispatcher: ?Dispatcher = null,
+    };
+}
+
+pub fn Part(comptime A: type) type {
+    return struct {
+        action: ?A,
+        glob: ?*Part(A),
+        glob_all: bool,
+        param_part: ?*Part(A),
+        param_names: ?[][]const u8,
+        parts: StringHashMap(Part(A)),
+
+        const Self = @This();
     };
 }
