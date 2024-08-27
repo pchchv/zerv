5364b0ad43cedb9afe68b7a949987aec0a31447d
pchchv <ipchchv@gmail.com>
2024-08-27 20:19:14 +0300

    Implement a routes tests

--- src/router_test.zig
+++ src/router_test.zig
@@ -4,3 +4,9 @@ const Request = @import("request.zig");
 const Response = @import("response.zig");
 
 fn testDispatcher1(_: zerv.Action(void), _: *Request, _: *Response) anyerror!void {}
+fn testRoute1(_: *Request, _: *Response) anyerror!void {}
+fn testRoute2(_: *Request, _: *Response) anyerror!void {}
+fn testRoute3(_: *Request, _: *Response) anyerror!void {}
+fn testRoute4(_: *Request, _: *Response) anyerror!void {}
+fn testRoute5(_: *Request, _: *Response) anyerror!void {}
+fn testRoute6(_: *Request, _: *Response) anyerror!void {}
