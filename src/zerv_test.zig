f3852b49e07ca527373e16c0f68745d449c9a24e
pchchv <ipchchv@gmail.com>
2024-08-30 11:19:40 +0300

    Implement an init and a deinit methods

--- src/zerv_test.zig
+++ src/zerv_test.zig
@@ -16,4 +16,16 @@ const TestMiddleware = struct {
     allocator: Allocator,
     v1: []const u8,
     v2: []const u8,
+
+    fn init(config: TestMiddleware.Config, mc: MiddlewareConfig) !TestMiddleware {
+        return .{
+            .allocator = mc.allocator,
+            .v1 = try std.fmt.allocPrint(mc.arena, "tm1-{d}", .{config.id}),
+            .v2 = try std.fmt.allocPrint(mc.allocator, "tm2-{d}", .{config.id}),
+        };
+    }
+
+    pub fn deinit(self: *const TestMiddleware) void {
+        self.allocator.free(self.v2);
+    }
 };
