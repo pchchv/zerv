2bf35a502fd9d195485279358b4132856f03d7f2
pchchv <ipchchv@gmail.com>
2024-09-02 09:23:41 +0300

    invalid request and invalid request path tests

--- src/zerv_test.zig
+++ src/zerv_test.zig
@@ -454,3 +454,21 @@ test "zerv: quick shutdown" {
     thrd.join();
     server.deinit();
 }
+
+test "zerv: invalid request" {
+    const stream = testStream(5992);
+    defer stream.close();
+    try stream.writeAll("TEA / HTTP/1.1\r\n\r\n");
+
+    var buf: [100]u8 = undefined;
+    try t.expectString("HTTP/1.1 400 \r\nConnection: Close\r\nContent-Length: 15\r\n\r\nInvalid Request", testReadAll(stream, &buf));
+}
+
+test "zerv: invalid request path" {
+    const stream = testStream(5992);
+    defer stream.close();
+    try stream.writeAll("TEA /hello\rn\nWorld:test HTTP/1.1\r\n\r\n");
+
+    var buf: [100]u8 = undefined;
+    try t.expectString("HTTP/1.1 400 \r\nConnection: Close\r\nContent-Length: 15\r\n\r\nInvalid Request", testReadAll(stream, &buf));
+}
