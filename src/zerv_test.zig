aae3f3bc937822f285d62dbc57b44f94e11e4c3c
pchchv <ipchchv@gmail.com>
2024-09-02 09:40:46 +0300

    Add route params tests

--- src/zerv_test.zig
+++ src/zerv_test.zig
@@ -550,3 +550,12 @@ test "zerv: unhandled exception with custom error handler" {
     var buf: [150]u8 = undefined;
     try t.expectString("HTTP/1.1 500 \r\nstate: 3\r\nerr: TestUnhandledError\r\nContent-Length: 29\r\n\r\n#/why/arent/tags/hierarchical", testReadAll(stream, &buf));
 }
+
+test "zerv: route params" {
+    const stream = testStream(5993);
+    defer stream.close();
+    try stream.writeAll("GET /api/v2/users/9001 HTTP/1.1\r\n\r\n");
+
+    var buf: [100]u8 = undefined;
+    try t.expectString("HTTP/1.1 200 \r\nContent-Length: 20\r\n\r\nversion=v2,user=9001", testReadAll(stream, &buf));
+}
