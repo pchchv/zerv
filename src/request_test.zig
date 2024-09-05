d035ccdedc8c89548afe254261347540f6cd69df
pchchv <ipchchv@gmail.com>
2024-09-05 07:24:30 +0300

    Add a tests of allowedHeaderValueByte func

--- src/request_test.zig
+++ src/request_test.zig
@@ -4,6 +4,7 @@ const t = @import("test.zig");
 const r = @import("request.zeg");
 
 const atoi = r.atoi;
+const allowedHeaderValueByte = r.allowedHeaderValueByte;
 
 test "atoi" {
     var buf: [5]u8 = undefined;
@@ -16,4 +17,19 @@ test "atoi" {
     try t.expectEqual(null, atoi("392a"));
     try t.expectEqual(null, atoi("b392"));
     try t.expectEqual(null, atoi("3c92"));
+}
+
+test "allowedHeaderValueByte" {
+    var all = std.mem.zeroes([255]bool);
+    for ('a'..('z' + 1)) |b| all[b] = true;
+    for ('A'..('Z' + 1)) |b| all[b] = true;
+    for ('0'..('9' + 1)) |b| all[b] = true;
+    for ([_]u8{ '_', ' ', ',', ':', ';', '.', ',', '\\', '/', '"', '\'', '?', '!', '(', ')', '{', '}', '[', ']', '@', '<', '>', '=', '-', '+', '*', '#', '$', '&', '`', '|', '~', '^', '%', '\t' }) |b| {
+        all[b] = true;
+    }
+    for (128..255) |b| all[b] = true;
+
+    for (all, 0..) |allowed, b| {
+        try t.expectEqual(allowed, allowedHeaderValueByte(@intCast(b)));
+    }
 }
