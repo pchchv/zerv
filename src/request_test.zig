2aebad01ad66e49d71396bc0b45316310ccebe30
pchchv <ipchchv@gmail.com>
2024-09-05 07:31:01 +0300

    Add parsing tests

--- src/request_test.zig
+++ src/request_test.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const t = @import("test.zig");
+const zerv = @import("zerv.zig");
 const Request = @import("request.zeg");
 
 const Config = @import("config.zig").Config.Request;
@@ -65,6 +66,99 @@ test "request: parse headers" {
         try t.expectString("pondzpondz.com", r.header("host").?);
         try t.expectString("Some-Value", r.header("misc").?);
         try t.expectString("none", r.header("authorization").?);
+    }
+}
+
+test "request: parse method" {
+    defer t.reset();
+    {
+        try expectParseError(error.UnknownMethod, "GETT / HTTP/1.1 ", .{});
+        try expectParseError(error.UnknownMethod, " PUT / HTTP/1.1", .{});
+    }
+
+    {
+        const r = try testParse("GET / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.GET, r.method);
+    }
+
+    {
+        const r = try testParse("PUT / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.PUT, r.method);
+    }
+
+    {
+        const r = try testParse("POST / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.POST, r.method);
+    }
+
+    {
+        const r = try testParse("HEAD / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.HEAD, r.method);
+    }
+
+    {
+        const r = try testParse("PATCH / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.PATCH, r.method);
+    }
+
+    {
+        const r = try testParse("DELETE / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.DELETE, r.method);
+    }
+
+    {
+        const r = try testParse("OPTIONS / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Method.OPTIONS, r.method);
+    }
+}
+
+test "request: parse request target" {
+    defer t.reset();
+    {
+        try expectParseError(error.InvalidRequestTarget, "GET NOPE", .{});
+        try expectParseError(error.InvalidRequestTarget, "GET nope ", .{});
+        try expectParseError(error.InvalidRequestTarget, "GET http://www.pondzpondz.com/test ", .{}); // this should be valid
+        try expectParseError(error.InvalidRequestTarget, "PUT hello ", .{});
+        try expectParseError(error.InvalidRequestTarget, "POST  /hello ", .{});
+        try expectParseError(error.InvalidRequestTarget, "POST *hello ", .{});
+    }
+
+    {
+        const r = try testParse("PUT / HTTP/1.1\r\n\r\n", .{});
+        try t.expectString("/", r.url.raw);
+    }
+
+    {
+        const r = try testParse("PUT /api/v2 HTTP/1.1\r\n\r\n", .{});
+        try t.expectString("/api/v2", r.url.raw);
+    }
+
+    {
+        const r = try testParse("DELETE /API/v2?hack=true&over=9000%20!! HTTP/1.1\r\n\r\n", .{});
+        try t.expectString("/API/v2?hack=true&over=9000%20!!", r.url.raw);
+    }
+
+    {
+        const r = try testParse("PUT * HTTP/1.1\r\n\r\n", .{});
+        try t.expectString("*", r.url.raw);
+    }
+}
+
+test "request: parse protocol" {
+    defer t.reset();
+    {
+        try expectParseError(error.UnknownProtocol, "GET / http/1.1\r\n", .{});
+        try expectParseError(error.UnsupportedProtocol, "GET / HTTP/2.0\r\n", .{});
+    }
+
+    {
+        const r = try testParse("PUT / HTTP/1.0\r\n\r\n", .{});
+        try t.expectEqual(zerv.Protocol.HTTP10, r.protocol);
+    }
+
+    {
+        const r = try testParse("PUT / HTTP/1.1\r\n\r\n", .{});
+        try t.expectEqual(zerv.Protocol.HTTP11, r.protocol);
     }
 }
 
