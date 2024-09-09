b98451265f3a22542212e3d41982749e2f977c56
pchchv <ipchchv@gmail.com>
2024-09-09 12:25:11 +0300

    Add a small large tests

--- src/thread_pool_test.zig
+++ src/thread_pool_test.zig
@@ -23,6 +23,22 @@ test "ThreadPool: small fuzz" {
     try t.expectEqual(50_000, testSum);
 }
 
+test "ThreadPool: large fuzz" {
+    defer t.reset();
+
+    testSum = 0; // global defined near the end of this file
+    var tp = try thp.ThreadPool(testIncr).init(t.arena.allocator(), .{ .count = 50, .backlog = 1000, .buffer_size = 512 });
+
+    for (0..50_000) |_| {
+        tp.spawn(.{1});
+    }
+    while (tp.empty() == false) {
+        std.time.sleep(std.time.ns_per_ms);
+    }
+    tp.stop();
+    try t.expectEqual(50_000, testSum);
+}
+
 fn testIncr(c: u64, buf: []u8) void {
     std.debug.assert(buf.len == 512);
     _ = @atomicRmw(u64, &testSum, .Add, c, .monotonic);
