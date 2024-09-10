e44a6932df6c7f00a83cc8713baf5298c23a6b0d
pchchv <ipchchv@gmail.com>
2024-09-10 15:26:22 +0300

    Implement a hellp and a json functions

--- examples/basic.zig
+++ examples/basic.zig
@@ -12,3 +12,16 @@ fn index(_: *zerv.Request, res: *zerv.Response) !void {
         \\ <li><a href="/explicit_write">Explicit Write</a>
     ;
 }
+
+fn hello(req: *zerv.Request, res: *zerv.Response) !void {
+    const query = try req.query();
+    const name = query.get("name") orelse "stranger";
+    // Could also see res.writer(), see the writer endpoint for an example
+    res.body = try std.fmt.allocPrint(res.arena, "Hello {s}", .{name});
+}
+
+fn json(req: *zerv.Request, res: *zerv.Response) !void {
+    const name = req.param("name").?;
+    // the last parameter to res.json is an std.json.StringifyOptions
+    try res.json(.{ .hello = name }, .{});
+}
