c19bb76803634f54aa02ab6f0be4b11461bd769b
pchchv <ipchchv@gmail.com>
2024-09-11 12:30:52 +0300

    Implement a shutdown function

--- examples/shutdown.zig
+++ examples/shutdown.zig
@@ -4,7 +4,16 @@
 const std = @import("std");
 const zerv = @import("zerv");
 
+var server_instance: ?*zerv.Server(void) = null;
+
 fn index(_: *zerv.Request, res: *zerv.Response) !void {
     const writer = res.writer();
     return std.fmt.format(writer, "To shutdown, run:\nkill -s int {d}", .{std.c.getpid()});
+}
+
+fn shutdown(_: c_int) callconv(.C) void {
+    if (server_instance) |server| {
+        server_instance = null;
+        server.stop();
+    }
 }
