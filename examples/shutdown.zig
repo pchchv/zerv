7ad5a27a4f9f979bbaf601a667277f50c6a57c6c
pchchv <ipchchv@gmail.com>
2024-09-11 12:32:39 +0300

    Implement a shutdown example

--- examples/shutdown.zig
+++ examples/shutdown.zig
@@ -1,10 +1,47 @@
 // This example demonstrates how to shutdown zerv.
 // Only works on Linux/MacOS/BSD.
 
-const std = @import("std");
 const zerv = @import("zerv");
+const std = @import("std");
+
+const PORT = 8809;
 
 var server_instance: ?*zerv.Server(void) = null;
+
+pub fn main() !void {
+    if (comptime @import("builtin").os.tag == .windows) {
+        std.debug.print("This example does not run on Windows. Sorry\n", .{});
+        return error.PlatformNotSupported;
+    }
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // call shutdown function (below)
+    // when SIGINT or SIGTERM are received
+    std.posix.sigaction(std.posix.SIG.INT, &.{
+        .handler = .{ .handler = shutdown },
+        .mask = std.posix.empty_sigset,
+        .flags = 0,
+    }, null);
+    std.posix.sigaction(std.posix.SIG.TERM, &.{
+        .handler = .{ .handler = shutdown },
+        .mask = std.posix.empty_sigset,
+        .flags = 0,
+    }, null);
+
+    var server = try zerv.Server(void).init(allocator, .{ .port = PORT }, {});
+    defer server.deinit();
+
+    var router = server.router(.{});
+    router.get("/", index, .{});
+
+    std.debug.print("listening http://localhost:{d}/\n", .{PORT});
+    std.debug.print("process id (pid): {d}\n", .{std.c.getpid()});
+
+    server_instance = &server;
+    try server.listen();
+}
 
 fn index(_: *zerv.Request, res: *zerv.Response) !void {
     const writer = res.writer();
