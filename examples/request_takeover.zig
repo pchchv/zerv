4333811d40fb0354cafd25446bdc417c5fcc82b1
pchchv <ipchchv@gmail.com>
2024-09-12 10:22:22 +0300

    Implement a takeover request processing example

--- examples/request_takeover.zig
+++ examples/request_takeover.zig
@@ -1,7 +1,10 @@
 // This example uses the Handler's "handle" function to
 // completely takeover request processing from zerv.
 
+const std = @import("std");
 const zerv = @import("zerv");
+
+const PORT = 8805;
 
 const Handler = struct {
     pub fn handle(_: *Handler, _: *zerv.Request, res: *zerv.Response) void {
@@ -15,3 +18,25 @@ const Handler = struct {
         ;
     }
 };
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var handler = Handler{};
+    var server = try zerv.Server(*Handler).init(allocator, .{ .port = PORT }, &handler);
+
+    defer server.deinit();
+
+    // ensures a clean shutdown,
+    // finishing off any existing requests see shutdown.zig for how to
+    // to break server.listen with an interrupt
+    defer server.stop();
+
+    // Routes aren't used in this mode
+    std.debug.print("listening http://localhost:{d}/\n", .{PORT});
+
+    // Starts the server,
+    // this is blocking
+    try server.listen();
+}
